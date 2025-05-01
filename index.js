54b37f5bacda5e79393b99c83b129acdc4d9ad2a
Anton Kudryashov <driver_90@list.ru>
2025-05-01 17:28:04 +0700

    Исправление конфигурационных файлов

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const func = () => {
     console.log(sum(x, y)); // 30
     debugger;
     x = 20;
-    y = 30;
+    y = '30;
     
-    console.log(sum(x, y)); // 50
-    };
+     console.log(sum(x, y)); // 50
+  };
     
-    func();
+func()
